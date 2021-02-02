bf4fcfd0e433a97cd4e849b263245c6d162d6cff
Yevgeniy <anigeka@gmail.com>
2021-02-01 19:17:26 -0500

    add ability for asyncronous top-level comment"

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,19 +5,21 @@ class CommentsController < ApplicationController
     end
 
     def create
-        raise params.inspect
         if params[:comment][:parent_id].to_i > 0
             parent = Comment.find_by_id(params[:comment].delete(:parent_id))
-            comment = parent.children.build(comment_params)
+            @comment = parent.children.build(comment_params)
         else  
-            comment = Comment.new(comment_params)
+            @comment = Comment.new(comment_params)
         end
 
-        if comment.save
-            redirect_to root_path
-        else
-            flash[:alert] = "Could not save comment"
-            redirect_to item_path params[:item_id]
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to root_path }
+                format.js
+            else
+                format.html { redirect_to root_path }
+                format.js
+            end
         end
     end
 private
