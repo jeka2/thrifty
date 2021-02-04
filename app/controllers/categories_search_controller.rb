ac1f84dd80a2c19eb1c465a611808f27a5246ed8
Yevgeniy <anigeka@gmail.com>
2021-02-04 13:49:56 -0500

    category item search by  ids

--- app/controllers/categories_search_controller.rb
+++ app/controllers/categories_search_controller.rb
@@ -5,5 +5,15 @@ class CategoriesSearchController < ApplicationController
 
     def create
         binding.pry
+        ['2','3','6']
+        Item.includes([:categories]).where( {:hashed_id=>params[:id]} )
+    # Item.includes/joins([:categories]).where("category_id = params[:id][0] OR category_id = pararms[;id][1] OR categery")
+
+ #CategoryItems.where(caterogry_id: pararms[0] OR catergeroy)
+
+ #CategoryItems.all.select do |c|
+        if c.category_id == pararms[id][0] || c.category_id == params[id]
+            c.item
+        end 
     end
 end
