c56f5aba91cfd56485726d37a74998c6f14ea274
Yevgeniy <anigeka@gmail.com>
2021-02-16 12:16:00 -0500

    fix rating update/create issue

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,15 +2,15 @@ class RatingsController < ApplicationController
     include RatingsHelper
     before_action :log_in_if_not
     def create
-        item = Item.find_by_id(params[:item_id])
+        @item = Item.find_by_id(params[:item_id])
         star_count = params[:star].to_i
         
-        Rating.create(user_id: current_user.id, item_id: item.id, score: star_count)
+        Rating.create(user_id: current_user.id, item_id: @item.id, score: star_count)
 
-        update_rating(item: item, old_score: 0, new_score: star_count, new_record:true)
+        update_rating(item: @item, old_score: 0, new_score: star_count, new_record:true)
 
-        @new_rating = item.rating
-        @new_ratings_count = item.ratings_count
+        @new_rating = @item.rating
+        @new_ratings_count = @item.ratings_count
 
         respond_to do |format|
             format.html { redirect_to root_path }
